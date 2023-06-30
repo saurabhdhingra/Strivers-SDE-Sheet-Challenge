e04bebe9442b47a1aca7e448b42f86667770123a
saurabhdhingra <75309640+saurabhdhingra@users.noreply.github.com>
2023-07-01 00:43:59 +0530

    sdfgsdg

--- implementStackusingQueue.java
+++ implementStackusingQueue.java
@@ -2,13 +2,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class MyStack {
-     private Queue<Integer> queue;
+    private Queue<Integer> queue;
     public MyStack() {
          this.queue = new LinkedList<Integer>();
     }
     
     public void push(int x) {
-        queue.add(x);
+    queue.add(x);
        for(int i=0;i<queue.size()-1;i++)
        {
            queue.add(queue.poll());
